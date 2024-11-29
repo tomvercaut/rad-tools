2679376a1be7d6290fe33b605c0c890d093a5a9f
Tom Vercauteren <tomvercaut@gmail.com>
2024-11-29 23:55:54 +0100

    feat(eqd2): extract eqd2 module to separate file
    - Move `eqd2` function and its documentation from lib.rs to new eqd2.rs
    - Update module exports in lib.rs to reflect changes

--- bio_dose/src/lib.rs
+++ bio_dose/src/lib.rs
@@ -1,7 +1,7 @@
 mod bed;
 pub use bed::bed;
-
-use tracing::{error, instrument};
+mod eqd2;
+pub use eqd2::eqd2;
 
 #[derive(thiserror::Error, Debug, Clone)]
 pub enum Error {
@@ -11,115 +11,4 @@ pub enum Error {
     InvalidTotalFractions,
     #[error("Alpha beta ratio must be greater than zero")]
     InvalidAlphaBetaRatio,
-}
-
-/// Computes the equivalent dose in 2 Gy fractions (EQD2).
-///
-/// EQD2 is a way to compare different radiation therapy regimens by normalizing
-/// them to an equivalent dose given in 2 Gy fractions. This is useful for understanding and
-/// comparing the biological effect of different dosages and fractionation schemes.
-///
-/// # Arguments
-///
-/// * `dose_per_fraction` - dose delivered per fraction (in Gy).
-/// * `total_fractions` - total number of fractions.
-/// * `alpha_beta_ratio` - Dose (Gy) at which the linear and quadratic components of cell kill are equal.
-///
-/// # Returns
-///
-/// * `Result<f64, Error>` - The equivalent dose in 2 Gy fractions, or an error if any of the input
-///   parameters are invalid.
-///
-/// # Example
-///
-/// ```
-/// use rad_tools_bio_dose::eqd2;
-///
-/// let value = eqd2(3.0, 20, 3.0).unwrap();
-/// assert!((72.0 - value).abs() < 1e-6);
-///
-/// let value = eqd2(3.0, 20, 10.0).unwrap();
-/// assert!((65.0 - value).abs() < 1e-6);
-/// ```
-///
-///
-/// # References
-///
-/// This calculation is based on the linear quadratic model commonly used in radiobiology.
-/// More information can be found in [The linear-quadratic formula and progress in fractionated radiotherapy ](https://pubmed.ncbi.nlm.nih.gov/2670032/) and [21 years of Biologically Effective Dose](https://pmc.ncbi.nlm.nih.gov/articles/PMC3473681/)
-///
-#[instrument(level = "debug")]
-pub fn eqd2(
-    dose_per_fraction: f64,
-    total_fractions: u32,
-    alpha_beta_ratio: f64,
-) -> Result<f64, Error> {
-    if dose_per_fraction <= 0.0 {
-        error!(
-            "Dose per fraction ({:#?}) must be greater than zero",
-            dose_per_fraction
-        );
-        return Err(Error::InvalidDosePerFraction);
-    }
-    if total_fractions == 0 {
-        error!(
-            "Total number of fractions ({:#?}) must be greater than zero",
-            total_fractions
-        );
-        return Err(Error::InvalidTotalFractions);
-    }
-    if alpha_beta_ratio <= 0.0 {
-        error!(
-            "Alpha beta ratio ({:#?}) must be greater than zero",
-            alpha_beta_ratio
-        );
-        return Err(Error::InvalidAlphaBetaRatio);
-    }
-
-    let n = total_fractions as f64;
-    let d = dose_per_fraction;
-    Ok(n * d * (d + alpha_beta_ratio) / (2.0 + alpha_beta_ratio))
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_eqd2_valid_inputs() {
-        assert!((eqd2(3.0, 20, 3.0).unwrap() - 72.0).abs() < 1e-6);
-        assert!((eqd2(3.0, 20, 10.0).unwrap() - 65.0).abs() < 1e-6);
-    }
-
-    #[test]
-    fn test_eqd2_invalid_dose_per_fraction() {
-        match eqd2(0.0, 20, 3.0) {
-            Err(Error::InvalidDosePerFraction) => (),
-            _ => panic!("Expected InvalidDosePerFraction error"),
-        }
-        match eqd2(-1.0, 20, 3.0) {
-            Err(Error::InvalidDosePerFraction) => (),
-            _ => panic!("Expected InvalidDosePerFraction error"),
-        }
-    }
-
-    #[test]
-    fn test_eqd2_invalid_total_fractions() {
-        match eqd2(3.0, 0, 3.0) {
-            Err(Error::InvalidTotalFractions) => (),
-            _ => panic!("Expected InvalidTotalFractions error"),
-        }
-    }
-
-    #[test]
-    fn test_eqd2_invalid_alpha_beta_ratio() {
-        match eqd2(3.0, 20, 0.0) {
-            Err(Error::InvalidAlphaBetaRatio) => (),
-            _ => panic!("Expected InvalidAlphaBetaRatio error"),
-        }
-        match eqd2(3.0, 20, -1.0) {
-            Err(Error::InvalidAlphaBetaRatio) => (),
-            _ => panic!("Expected InvalidAlphaBetaRatio error"),
-        }
-    }
 }
