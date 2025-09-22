c6c5b61059821b210404b8138bc848a094532439
Tom Vercauteren <tomvercaut@gmail.com>
2025-09-23 01:14:54 +0200

    feat: added `dcm_dictionary` library
    - Introduced a new `dcm_dictionary` library with error handling and a centralized tag dictionary.
    - Implemented dictionary parsing and management via `TagDictionary` and `Item` structs.
    - Added support for common DICOM tag attributes (tag, VR, name, VM, version).
    - Integrated lazy initialization with a static `TAG_DICTIONARY`.

---
+++ dcm_dictionary/src/error.rs
@@ -0,0 +1,19 @@
+use dicom_core::header::ParseTagError;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("IO error")]
+    IO(#[from] std::io::Error),
+    #[error("Failed to parse DICOM tag")]
+    ParseTagFailed(#[from] ParseTagError),
+    #[error("Failed to parse DICOM VR")]
+    ParseVrFailed,
+    #[error("Failed to parse DICOM VM")]
+    ParseVmFailed,
+    #[error("Failed to parse DICOM version")]
+    ParseVersionFailed,
+    #[error("Failed to parse dictionary DICOM tag line")]
+    DictionaryTagLineFormatInvalid,
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
