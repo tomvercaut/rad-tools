2f93033b1e3095b58f47aaa7d681398767276ae2
Tom <tomvercaut@gmail.com>
2026-03-01 14:25:30 +0100

    feat(value): implement `DicomValue` and `to_dicom_object` macros for `SV`, `SS`, `ST`, `SH`, and `SL`
    - Added `to_dicom_object_for_number` and `to_dicom_object_for_numbers` macros for `SV`, `SS`, and `SL` types.
    - Added `to_dicom_object_for_string` and `to_dicom_object_for_strings` macros for `ST` and `SH` types.
    - Implemented `DicomValue` trait for `SV`, `SVs`, `SS`, `SSs`, `ST`, `STs`, `SH`, `SHs`, `SL`, and `SLs` with `InMemDicomObject` support.

--- dcm_data/src/value/value_sh.rs
+++ dcm_data/src/value/value_sh.rs
@@ -1,3 +1,6 @@
+use crate::{DicomValue, Value};
+use dicom_object::InMemDicomObject;
+
 crate::dicom_value_type!(SH, SH, String);
 crate::dicom_value_type!(SHs, SH, Vec<String>);
 crate::one_to_many_dicom_value_by_delim!(SH, SHs, '\\');
@@ -6,3 +9,8 @@ crate::from_dicom_object_for_strings!(SHs, SH, '\\');
 crate::dicom_value_from_str!(SH);
 crate::dicom_value_from_same_type!(SH, String);
 crate::dicom_value_from_same_type!(SHs, Vec<String>);
+crate::to_dicom_object_for_string!(SH, SH);
+crate::to_dicom_object_for_strings!(SHs, SH);
+
+impl<const G: u16, const E: u16> DicomValue<InMemDicomObject> for SH<G, E> {}
+impl<const G: u16, const E: u16> DicomValue<InMemDicomObject> for SHs<G, E> {}
--- dcm_data/src/value/value_st.rs
+++ dcm_data/src/value/value_st.rs
@@ -1,3 +1,6 @@
+use crate::{DicomValue, Value};
+use dicom_object::InMemDicomObject;
+
 crate::dicom_value_type!(ST, ST, String);
 crate::dicom_value_type!(STs, ST, Vec<String>);
 crate::one_to_many_dicom_value_by_delim!(ST, STs, '\\');
@@ -6,3 +9,8 @@ crate::from_dicom_object_for_strings!(STs, ST, '\\');
 crate::dicom_value_from_str!(ST);
 crate::dicom_value_from_same_type!(ST, String);
 crate::dicom_value_from_same_type!(STs, Vec<String>);
+crate::to_dicom_object_for_string!(ST, ST);
+crate::to_dicom_object_for_strings!(STs, ST);
+
+impl<const G: u16, const E: u16> DicomValue<InMemDicomObject> for ST<G, E> {}
+impl<const G: u16, const E: u16> DicomValue<InMemDicomObject> for STs<G, E> {}
