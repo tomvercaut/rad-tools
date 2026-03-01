0c89c6727136f09654ab407cf634b7ea1eec6329
Tom <tomvercaut@gmail.com>
2026-03-01 01:12:33 +0100

    **feat(value): add `DicomValue` implementation and `to_dicom_object` macros for `AS` and `ASs`**
    - Implemented `DicomValue` trait for `AS` and `ASs` with `InMemDicomObject` support.
    - Added `to_dicom_object_for_string` and `to_dicom_object_for_strings` macros to enable conversion of `AS` and `ASs` into DICOM objects.

--- dcm_data/src/value/value_as.rs
+++ dcm_data/src/value/value_as.rs
@@ -1,3 +1,6 @@
+use dicom_object::InMemDicomObject;
+use crate::{Value, DicomValue};
+
 crate::dicom_value_type!(AS, AS, String);
 crate::dicom_value_type!(ASs, AS, Vec<String>);
 crate::one_to_many_dicom_value_by_delim!(AS, ASs, '\\');
@@ -6,3 +9,8 @@ crate::from_dicom_object_for_strings!(ASs, AS, '\\');
 crate::dicom_value_from_str!(AS);
 crate::dicom_value_from_same_type!(AS, String);
 crate::dicom_value_from_same_type!(ASs, Vec<String>);
+crate::to_dicom_object_for_string!(AS, AS);
+crate::to_dicom_object_for_strings!(ASs, AS);
+
+impl<const G: u16, const E: u16> DicomValue<InMemDicomObject> for AS<G, E> {}
+impl<const G: u16, const E: u16> DicomValue<InMemDicomObject> for ASs<G, E> {}
